fdb8a7d4233836a6a78bc3031c0aa2378bcfa2d2
edward <edwardlfish@gmail.com>
2024-04-10 14:06:35 +0300

    Start developing of Accounting

---
+++ src/main/java/ait/cohort34/accounting/dto/RolesDto.java
@@ -0,0 +1,15 @@
+package ait.cohort34.accounting.dto;
+
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RolesDto {
+    String login;
+    @Singular
+    Set<String> roles;
+}
---
+++ src/main/java/ait/cohort34/accounting/dto/UserDto.java
@@ -0,0 +1,17 @@
+package ait.cohort34.accounting.dto;
+
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    String login;
+    String firstName;
+    String lastName;
+    @Singular
+    Set<String> roles;
+}
