ed39d68b89eb16aa1524d7f3f5986555967c7d6e
edward <edwardlfish@gmail.com>
2024-04-11 09:53:06 +0300

    Accounting homework

---
+++ src/main/java/ait/cohort34/accounting/service/UserAccountServiceImpl.java
@@ -0,0 +1,78 @@
+package ait.cohort34.accounting.service;
+
+import ait.cohort34.accounting.dao.UserAccountRepository;
+import ait.cohort34.accounting.dto.RolesDto;
+import ait.cohort34.accounting.dto.UserDto;
+import ait.cohort34.accounting.dto.UserEditDto;
+import ait.cohort34.accounting.dto.UserRegisterDto;
+import ait.cohort34.accounting.dto.exceptions.UserExistsException;
+import ait.cohort34.accounting.dto.exceptions.UserNotFoundException;
+import ait.cohort34.accounting.model.UserAccount;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserAccountServiceImpl implements UserAccountService {
+
+    final UserAccountRepository userAccountRepository;
+    final ModelMapper modelMapper;
+    @Override
+    public UserDto register(UserRegisterDto userRegisterDto) {
+        if (userAccountRepository.existsById(userRegisterDto.getLogin())) {
+            throw new UserExistsException();
+        }
+        UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
+        userAccountRepository.save(userAccount);
+        return modelMapper.map(userAccount, UserDto.class);
+    }
+
+    @Override
+    public UserDto getUser(String login) {
+        UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+        return modelMapper.map(userAccount, UserDto.class);
+    }
+
+    @Override
+    public UserDto removeUser(String login) {
+        UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+        userAccountRepository.delete(userAccount);
+        return modelMapper.map(userAccount, UserDto.class);
+    }
+
+    @Override
+    public UserDto updateUser(String login, UserEditDto userEditDto) {
+        UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+        if (userEditDto.getFirstName() != null) {
+            userAccount.setFirstName(userEditDto.getFirstName());
+        }
+        if (userEditDto.getLastName() != null) {
+            userAccount.setLastName(userEditDto.getLastName());
+        }
+        userAccountRepository.save(userAccount);
+        return modelMapper.map(userAccount, UserDto.class);
+    }
+
+    @Override
+    public RolesDto changeRolesList(String login, String role, boolean isAddRole) {
+        UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+        boolean res;
+        if (isAddRole) {
+            res = userAccount.addRole(role);
+        } else {
+            res = userAccount.removeRole(role);
+        }
+        if(res) {
+            userAccountRepository.save(userAccount);
+        }
+        return modelMapper.map(userAccount, RolesDto.class);
+    }
+
+    @Override
+    public void changePassword(String login, String newPassword) {
+        UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+        userAccount.setPassword(newPassword);
+        userAccountRepository.save(userAccount);
+    }
+}
