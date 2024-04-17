0e323d9767a6fb1518fc62a6818c31caad77c5c6
edward <edwardlfish@gmail.com>
2024-04-17 10:41:20 +0300

    Started Spring Security

--- src/main/java/ait/cohort34/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/ait/cohort34/accounting/service/UserAccountServiceImpl.java
@@ -12,11 +12,12 @@ import ait.cohort34.accounting.model.UserAccount;
 import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserAccountServiceImpl implements UserAccountService {
+public class UserAccountServiceImpl implements UserAccountService, CommandLineRunner {
 
     final UserAccountRepository userAccountRepository;
     final ModelMapper modelMapper;
@@ -85,5 +86,10 @@ public class UserAccountServiceImpl implements UserAccountService {
         String password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
         userAccount.setPassword(password);
         userAccountRepository.save(userAccount);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
